ab3ef8c3e327ce7a04c7967f5278a81cc8e5fb6f
edwin vakkachan <edwinvakkachan@gmail.com>
2026-03-30 13:08:14 +0530

    env variable

--- qbittorrent/torrentCleanUp.js
+++ qbittorrent/torrentCleanUp.js
@@ -1,15 +1,18 @@
 import { qb } from "./qb.js";
+import dotenv from "dotenv";
+dotenv.config();
+
 
 export async function getTorrents() {
   const { data } = await qb.get("/api/v2/torrents/info");
   const hashes=[];
-
+const time = process.env.TIME;
  for (const value of data){
   if(value.state=='metaDL' && value.total_size<1 && value.time_active>600) {
      console.log(`❌ metadata failed \n ${value.name}\n`)
     hashes.push(value.hash)
   }
-  if( value.state=='stalledDL' && ((value.time_active/3600)>5)) {
+  if( value.state=='stalledDL' && (value.time_active>time)) {
    console.log(`❌ Torrent stalled \n ${value.name}\n`)
    hashes.push(value.hash)
   }
